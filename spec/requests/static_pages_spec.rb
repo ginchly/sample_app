10c5666890ec7866b74b74caff36cbddd9f1c3af
Graham Hinchly <graham.hinchly@hotmail.co.uk>
2012-05-29 17:56:11 +0100

    Full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,9 +7,13 @@ describe "StaticPages" do
             visit '/static_pages/home'
             page.should have_selector('h1', :text => 'Sample App')
         end
-        it "should have the title 'home'" do
+        it "should have the base title" do
             visit '/static_pages/home'
-            page.should have_selector('title', :text => 'Home')
+            page.should have_selector('title', :text => 'Ruby on Rails Sample App')
+        end
+        it "should not have a custom title" do
+            visit '/static_pages/home'
+            page.should_not have_selector('title', :text => 'Home')
         end
     end
     
