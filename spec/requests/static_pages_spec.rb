4944e86d39672bdcb9b42940fcaba82313928fc8
Graham Hinchly <graham.hinchly@hotmail.co.uk>
2012-05-30 10:47:05 +0100

    Added named routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,41 +4,52 @@ describe "StaticPages" do
   
     describe "Home Page" do
         it "should have the h1 sample app" do
-            visit '/static_pages/home'
+            visit root_path
             page.should have_selector('h1', :text => 'Sample App')
         end
         it "should have the base title" do
-            visit '/static_pages/home'
+            visit root_path
             page.should have_selector('title', :text => 'Ruby on Rails Sample App')
         end
         it "should not have a custom title" do
-            visit '/static_pages/home'
+            visit root_path
             page.should_not have_selector('title', :text => 'Home')
         end
     end
     
     describe "Help Page" do
         it "should have the h1 help" do
-            visit '/static_pages/help'
+            visit help_path
             page.should have_selector('h1', :text => 'Help')
         end
         it "should have the title 'help'" do
-            visit '/static_pages/help'
+            visit help_path
             page.should have_selector('title', :text => 'Help')
         end
     end
     
     describe "About Page" do
         it "should have the h1 'about us'" do
-            visit '/static_pages/about'
+            visit about_path
             page.should have_selector('h1', :text => 'About Us')
         end
         it "should have the title 'about us'" do
-            visit '/static_pages/about'
+            visit about_path
             page.should have_selector('title', :text => 'About Us')
         end
     end
 
+    describe "Contact Page" do
+      it "should have the h1 'contact" do
+        visit contact_path
+        page.should have_selector('h1', text: 'Contact')
+      end
+      
+      it "should have title 'contact" do
+        visit contact_path
+        page.should have_selector('title', text: "Contact")
+      end
+    end
 
 
 end
